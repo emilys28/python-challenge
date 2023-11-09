e34cfcff857a3fecebebe1fd878937a29cec9c1c
Emily Sims <esims28.97@gmail.com>
2023-11-09 17:28:31 -0500

    Added instructions and title formatting

---
+++ PyBank/main.py
@@ -0,0 +1,20 @@
+#Add formatting to look like example
+print("Financial Analysis")
+print("----------------------------")
+
+# The total number of months included in the dataset
+
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+
+# The greatest increase in profits (date and amount) over the entire period
+
+
+# The greatest decrease in profits (date and amount) over the entire period
+
+
+# your final script should both print the analysis to the terminal and 
+# export a text file with the results.
---
+++ PyPoll/main.py
@@ -0,0 +1,16 @@
+print("Election Results")
+print("-------------------------")
+# The total number of votes cast
+
+
+
+print("-------------------------")
+# A complete list of candidates who received votes
+
+# The percentage of votes each candidate won
+
+# The total number of votes each candidate won
+
+
+print("-------------------------")
+# The winner of the election based on popular vote
